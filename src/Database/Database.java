7f7567b1cc4b07f031d13e40fb1f95714765e244
rathodr2002 <100895212+rathodr2002@users.noreply.github.com>
2025-02-12 22:49:55 +0530

    Updated Database.java

--- src/Database/Database.java
+++ src/Database/Database.java
@@ -6,8 +6,8 @@ import java.sql.SQLException;
 
 public class Database {
     private static final String URL = "jdbc:mysql://127.0.0.1:3306/studentdb";
-    private static final String USER = "root";
-    private static final String PASSWORD = "Rathod@1801";
+    private static final String USER = "your_user_name";
+    private static final String PASSWORD = "your_password";
 
     public static Connection getConnection() {
         Connection conn = null;
